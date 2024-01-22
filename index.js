b9f3d6fa9bfc5262cc999381b4c7a40c10242064
Akshit Singla <akshit@akshits-mbp.lan>
2024-01-22 17:45:34 +0530

    Updating to add POST request

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var http = new require('http');
 
-var app = http.createServer(function(req, res) {
-	res.write('Hello World!');
-	res.end();
-  });  
+var app = http.createServer(function(request, response) {
+	console.dir(request.param)
+
+	if (request.method == 'POST') {
+		console.log('POST')
+		var body = ''
+		request.on('data', function(data) {
+			body += data
+			console.log('Partial body: ' + body)
+		})
+		request.on('end', function() {
+			console.log('Body: ' + body)
+			response.writeHead(200, {'Content-Type': 'text/html'})
+			response.end(`Hello POST! \n ${body}`)
+		})
+	} else {
+		console.log('GET')
+		response.writeHead(200, {'Content-Type': 'text/html'})
+		response.end(`Hello GET!`)
+	}
+});  
 
 app.listen(3000);
 console.log('API running on port 3000.');
